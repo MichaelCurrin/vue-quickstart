1fcd2da62f487e6d8f6d2e22b890242cde50adec
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-06-08 21:12:26 +0200

    feat: remove Babel plugin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,14 +42,6 @@
     <ul>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
